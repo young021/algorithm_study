93a08cc9fd61be6caf8bbe10989c9261b6bd7b1a
Tedhoon <gt0305@likelion.org>
2019-07-03 22:54:31 +0900

    first_algo

---
+++ algo1.py
@@ -0,0 +1,16 @@
+# 가운데 글자 가져오기....입니다...
+# 금요일에 발전해서 오겠씀다
+
+
+def solution(s):
+    a=len(s)
+    if a%2==1:
+        return s[a//2]
+    else :
+        return s[a//2-1]+s[a//2]
+    
+print('return') #string출력
+
+
+
+
